1a2f2034ea13edf3f4627c2947eb810c72dc06fb
Rafael Oliveira Ribeiro <38961393+rafribeiro@users.noreply.github.com>
2024-11-26 12:08:14 +0000

    update ip and port config on app.py

--- app.py
+++ app.py
@@ -44,4 +44,5 @@ def survey(name, email, index):
     return render_template("survey.html", image=image, index=index, total=len(images))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
